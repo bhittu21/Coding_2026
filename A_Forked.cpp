a12bf472645394620707e24135d069d2f5494b05
SK ABIR ALI <skabirali.study@gmail.com>
2026-01-01 22:59:09 +0600

    Add files via upload

---
+++ A_Forked.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test_case;
+    cin >> test_case;
+
+    while (test_case--) {
+        
+        int possible_of_KnightX,possible_of_KnightY;
+        cin >> possible_of_KnightX >> possible_of_KnightY;
+
+        int pos_of_kingX, pos_of_kingY;
+        cin >> pos_of_kingX >> pos_of_kingY;
+
+        int pos_of_queenX, pos_of_queenY;
+        cin >> pos_of_queenX >> pos_of_queenY;
+
+        
+    }
+
+    return 0;
+}
