709651bd8027c1003944a67585987760f325db57
bhittu21 <skabirali.study@gmail.com>
2026-01-12 12:25:19 +0600

    Div 3

---
+++ A_Blackslex_and_Password.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test_case;
+    cin >> test_case;
+
+    while (test_case--) {
+        int x,y;
+        cin >> x >> y;
+
+        cout << x*y+1 << endl;
+
+        
+    }
+
+    return 0;
+}
