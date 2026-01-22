99c1b4ac650de499c57783eddeb1d130109bd47a
bhittu21 <219447337+bhittu21@users.noreply.github.com>
2026-01-22 22:13:56 +0600

    Funn

---
+++ B_Was_it_Rated.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    if(n==15 || n==20 || n==25){
+        cout << "YES" << endl;
+        return 0;
+    } 
+
+    cout << "NO" << endl;
+
+    return 0;
+}
