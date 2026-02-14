39b91e2f734af2fdcf268497fcc3f3c23bc765ca
bhittu21 <219447337+bhittu21@users.noreply.github.com>
2026-02-14 16:18:44 +0600

    Damn

---
+++ Codeforces/A_GCD_vs_LCM.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test_case;
+    cin >> test_case;
+
+    while (test_case--) {
+        int x;
+        cin >> x;
+
+        cout << x-3 << " 1 1 1\n";
+        
+    }
+
+    return 0;
+}
